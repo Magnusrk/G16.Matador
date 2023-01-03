e01a2ea2257274c91f2dfc9180a2751be2b3346d
Magnus <magnusrk90@gmail.com>
2023-01-03 11:52:35 +0100

    Updated MoneyBalance

--- src/main/java/G16/PlayerUtils/MoneyBalance.java
+++ src/main/java/G16/PlayerUtils/MoneyBalance.java
@@ -1,11 +1,11 @@
 package G16.PlayerUtils;
 
 public class MoneyBalance {
-    private int startingbalance;
+    private final int STARTINGBALANCE = 30000;
     private int balance;
 
-    public void setBalance(int balance) {
-        this.balance = balance;
+    public void setStartingBalance() {
+        balance = STARTINGBALANCE;
     }
     public void updateMoney(int money){
         balance+=money;

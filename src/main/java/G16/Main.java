34c036ab22972bd8b05c2a923800f1c40d7c26bb
Magnus <magnusrk90@gmail.com>
2023-01-03 11:30:40 +0100

    Added GUI skeleton
    Signed-off-by: Magnus <magnusrk90@gmail.com>

--- src/main/java/G16/Main.java
+++ src/main/java/G16/Main.java
@@ -1,9 +1,9 @@
 package G16;
+import G16.Graphics.MatadorGUI;
 import gui_main.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
-
+        MatadorGUI mGUI = new MatadorGUI();
     }
 }

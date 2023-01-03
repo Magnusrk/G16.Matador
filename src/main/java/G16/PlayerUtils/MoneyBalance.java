88e498dbf0aaa83a69a8f5cb68870f7065ea9e74
Magnus <magnusrk90@gmail.com>
2023-01-03 11:48:27 +0100

    Created PlayerUtils branch

--- src/main/java/G16/PlayerUtils/MoneyBalance.java
+++ src/main/java/G16/PlayerUtils/MoneyBalance.java
@@ -1,4 +1,10 @@
 package G16.PlayerUtils;
 
 public class MoneyBalance {
+    private int startingbalance;
+    private int balance;
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }

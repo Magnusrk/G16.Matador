5ddc4a63d25e36419812b3a4c7c9024f1dc8285d
MathiasW <s224293@student.dtu.dk>
2023-01-05 14:00:29 +0100

    Started making double rent method

--- src/main/java/G16/Controllers/GameController.java
+++ src/main/java/G16/Controllers/GameController.java
@@ -60,251 +60,260 @@ public class GameController {
     public void setupPlayers() {
         int playerCount = mgui.requestInteger(Language.getString("howManyPlayers"), MIN_PLAYERS, MAX_PLAYERS);
         while (players.size() < playerCount) {
-        int playerNum = 0;
-        while (players.size() < playerCount){
-            String newPlayerName = mgui.requestString(Language.getString("playerName"));
-            Player newPlayer = new Player();
-            newPlayer.setName(newPlayerName);
-            newPlayer.setID(players.size());
-            players.add(newPlayer);
-            mgui.addPlayer(newPlayerName, newPlayer.getPlayerBalance(),playerNum);
-            mgui.drawPlayerPosition(newPlayer);
-            playerNum++;
+            int playerNum = 0;
+            while (players.size() < playerCount) {
+                String newPlayerName = mgui.requestString(Language.getString("playerName"));
+                Player newPlayer = new Player();
+                newPlayer.setName(newPlayerName);
+                newPlayer.setID(players.size());
+                players.add(newPlayer);
+                mgui.addPlayer(newPlayerName, newPlayer.getPlayerBalance(), playerNum);
+                mgui.drawPlayerPosition(newPlayer);
+                playerNum++;
+            }
+            mgui.showMessage("Tryk OK for at starte!");
         }
-        mgui.showMessage("Tryk OK for at starte!");
     }
+        public void playTurn () {
 
-    public void playTurn() {
-
-        Player currentPlayer = players.get(currentPlayerID);
-        if (!currentPlayer.getBankrupt()) {
+            Player currentPlayer = players.get(currentPlayerID);
+            if (!currentPlayer.getBankrupt()) {
 
 
-            if (currentPlayer.getJailed()) {
-                inJail(currentPlayer);
+                if (currentPlayer.getJailed()) {
+                    inJail(currentPlayer);
+                } else {
+                    throwAndMove(currentPlayer);
+                }
+
+
+                //Update balance and position on GUI
+
+                mgui.updatePlayerBalance(currentPlayer);
+                if (currentPlayer.getBankrupt()) {
+                    removeowner(currentPlayer);
+                    mgui.removecar(currentPlayer);
+                }
+
+            }
+            setWinnerfound();
+            currentPlayerID += 1;
+            if (currentPlayerID >= players.size()) {
+                currentPlayerID = 0;
+
+            }
+
+            if (!TEST_MODE) {
+                if (!winnerfound) {
+                    playTurn();
+                }
+            }
+
+        }
+
+        private void setWinnerfound () {
+            int deadplayers = 0;
+            Player potwinner = null;
+            for (Player player : players) {
+                if (player.getBankrupt()) {
+                    deadplayers++;
+                } else {
+                    potwinner = player;
+                }
+            }
+            if (deadplayers == players.size() - 1) {
+                winnerfound = true;
+                mgui.showMessage(potwinner + " " + Language.getString("winner"));
+            }
+        }
+
+        public boolean getWinnerfound () {
+            return winnerfound;
+        }
+
+        public Player getCurrentplayer () {
+            return players.get(currentPlayerID);
+        }
+
+        private void throwAndMove (Player currentPlayer){
+            //Throw Dice
+            mgui.showMessage(currentPlayer.getName() + " kast med terningen!");
+            int[] diceThrow = Die.throwDice();
+            int diceSum = diceThrow[0] + diceThrow[1];
+
+            mgui.drawDice(diceThrow[0], diceThrow[1]);
+
+
+            //Move player
+            movePlayer(currentPlayer, diceSum);
+            mgui.drawPlayerPosition(currentPlayer);
+            landOnField(currentPlayer);
+        }
+
+        public void balance (Player player,int add){
+            player.addBalance(add);
+            mgui.updatePlayerBalance(player);
+        }
+
+        public void movePlayer (Player player,int moves){
+            int currentPosition = player.getPlayerPosition();
+            int newPosition = currentPosition + moves;
+            if (newPosition >= numberOfFields) {
+                newPosition -= numberOfFields;
+                giveStartMoney(player);
+            }
+            player.setPlayerPosition(newPosition);
+
+        }
+
+        public void landOnField (Player player){
+            Field currentfield = fields[player.getPlayerPosition()];
+            if (currentfield instanceof GoToJail) {
+                goToJail(player);
+            } else if (currentfield instanceof BuyableField prop) {
+                if (prop.getOwner() == null) {
+                    buyPropfield(player, prop);
+                } else {
+                    payRent(player, prop);
+                }
+            } else if (currentfield instanceof Tax tax) {
+                mgui.showMessage("Du betaler skat");
+                player.addBalance(-tax.getTax());
+            }
+
+        }
+
+        public void giveStartMoney (Player player){
+            player.addBalance(passStartReward);
+
+        }
+
+        public void goToJail (Player player){
+            player.setPlayerPosition(10);
+            player.setJailed(true);
+            mgui.drawPlayerPosition(player);
+        }
+
+        public void inJail (Player player){
+            String response;
+            if (player.getOutOfJailCards() > 0) {
+                response = mgui.requestUserButton((Language.getString("injail")), Language.getString("injailcard"), Language.getString("injailikkecard"));
+                if (response.equals(Language.getString("injailikkecard"))) {
+                    mgui.requestUserButton((Language.getString("injail")), Language.getString("injailpay"), Language.getString("injaildie"));
+                } else {
+                    player.setJailed(false);
+                    player.addOutOfJailCard(-1);
+                    mgui.showMessage(Language.getString("brugtkort"));
+                    throwAndMove(player);
+                    return;
+                }
             } else {
-                throwAndMove(currentPlayer);
+                response = mgui.requestUserButton((Language.getString("injail")), Language.getString("injailpay"), Language.getString("injaildie"));
             }
 
-
-            //Update balance and position on GUI
-
-            mgui.updatePlayerBalance(currentPlayer);
-            if (currentPlayer.getBankrupt()) {
-                removeowner(currentPlayer);
-                mgui.removecar(currentPlayer);
-            }
-
-        }
-        setWinnerfound();
-        currentPlayerID += 1;
-        if (currentPlayerID >= players.size()) {
-            currentPlayerID = 0;
-
-        }
-
-        if (!TEST_MODE) {
-            if (!winnerfound) {
-                playTurn();
-            }
-        }
-
-    }
-
-    private void setWinnerfound() {
-        int deadplayers = 0;
-        Player potwinner = null;
-        for (Player player : players) {
-            if (player.getBankrupt()) {
-                deadplayers++;
-            } else {
-                potwinner = player;
-            }
-        }
-        if (deadplayers == players.size() - 1) {
-            winnerfound = true;
-            mgui.showMessage(potwinner + " " + Language.getString("winner"));
-        }
-    }
-
-    public boolean getWinnerfound() {
-        return winnerfound;
-    }
-
-    public Player getCurrentplayer() {
-        return players.get(currentPlayerID);
-    }
-
-    private void throwAndMove(Player currentPlayer) {
-        //Throw Dice
-        mgui.showMessage(currentPlayer.getName() + " kast med terningen!");
-        int[] diceThrow = Die.throwDice();
-        int diceSum = diceThrow[0] + diceThrow[1];
-
-        mgui.drawDice(diceThrow[0], diceThrow[1]);
-
-
-        //Move player
-        movePlayer(currentPlayer, diceSum);
-        mgui.drawPlayerPosition(currentPlayer);
-        landOnField(currentPlayer);
-    }
-
-    public void balance(Player player, int add) {
-        player.addBalance(add);
-        mgui.updatePlayerBalance(player);
-    }
-
-    public void movePlayer(Player player, int moves) {
-        int currentPosition = player.getPlayerPosition();
-        int newPosition = currentPosition + moves;
-        if (newPosition >= numberOfFields) {
-            newPosition -= numberOfFields;
-            giveStartMoney(player);
-        }
-        player.setPlayerPosition(newPosition);
-
-    }
-
-    public void landOnField(Player player) {
-        Field currentfield = fields[player.getPlayerPosition()];
-        if (currentfield instanceof GoToJail) {
-            goToJail(player);
-        } else if (currentfield instanceof BuyableField prop) {
-            if (prop.getOwner() == null) {
-                buyPropfield(player, prop);
-            } else {
-                payRent(player, prop);
-            }
-        } else if (currentfield instanceof Tax tax) {
-            mgui.showMessage("Du betaler skat");
-            player.addBalance(-tax.getTax());
-        }
-
-    }
-
-    public void giveStartMoney(Player player) {
-        player.addBalance(passStartReward);
-
-    }
-
-    public void goToJail(Player player) {
-        player.setPlayerPosition(10);
-        player.setJailed(true);
-        mgui.drawPlayerPosition(player);
-    }
-
-    public void inJail(Player player) {
-        String response;
-        if (player.getOutOfJailCards() > 0) {
-            response = mgui.requestUserButton((Language.getString("injail")), Language.getString("injailcard"), Language.getString("injailikkecard"));
-            if (response.equals(Language.getString("injailikkecard"))) {
-                mgui.requestUserButton((Language.getString("injail")), Language.getString("injailpay"), Language.getString("injaildie"));
-            } else {
+            if (response.equals(Language.getString("injailpay"))) {
+                player.addBalance(-1000);
                 player.setJailed(false);
-                player.addOutOfJailCard(-1);
-                mgui.showMessage(Language.getString("brugtkort"));
+                mgui.showMessage(Language.getString("betalt"));
                 throwAndMove(player);
-                return;
-            }
-        } else {
-            response = mgui.requestUserButton((Language.getString("injail")), Language.getString("injailpay"), Language.getString("injaildie"));
-        }
-
-        if (response.equals(Language.getString("injailpay"))) {
-            player.addBalance(-1000);
-            player.setJailed(false);
-            mgui.showMessage(Language.getString("betalt"));
-            throwAndMove(player);
-        } else {
-
-            int[] die = Die.throwDice();
-            mgui.drawDice(die[0], die[1]);
-
-            if (die[0] == die[1]) {
-                player.setJailed(false);
-                movePlayer(player, die[0] + die[1]);
-                landOnField(player);
-                mgui.showMessage(Language.getString("2ens"));
             } else {
-                player.increaseTurnsinjail();
-                mgui.showMessage(Language.getString("ikke2ens"));
-                if (player.getTurnsinjail() > 2) {
-                    player.addBalance(-1000);
+
+                int[] die = Die.throwDice();
+                mgui.drawDice(die[0], die[1]);
+
+                if (die[0] == die[1]) {
                     player.setJailed(false);
                     movePlayer(player, die[0] + die[1]);
                     landOnField(player);
-                    mgui.showMessage(Language.getString("3ture"));
-                }
-            }
-        }
-    }
-
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
-
-    public void buyPropfield(Player currentplayer, BuyableField currentfield) {
-        if (currentfield.getPrice() < currentplayer.getPlayerBalance()) {
-            String resuslt = mgui.requestUserButton(Language.getString("prop"), Language.getString("yesTxt"), Language.getString("noTxt"));
-            if (resuslt == Language.getString("yesTxt")) {
-                currentplayer.addBalance(-currentfield.getPrice());
-                currentfield.setOwner(currentplayer);
-                mgui.setOwner(currentfield, currentplayer.getPlayerPosition());
-            }
-        } else {
-            mgui.showMessage("propikkeråd");
-        }
-    }
-
-    public void payRent(Player currentplayer, BuyableField currentfield) {
-        mgui.showMessage(Language.getString("payrent") + " " + currentfield.getOwner());
-
-        if (currentfield.getRent(0) < currentplayer.getPlayerBalance()) {
-            currentplayer.addBalance(-currentfield.getRent(0));
-            currentfield.getOwner().addBalance(currentfield.getRent(0));
-        } else {
-            currentplayer.addBalance(-currentplayer.getPlayerBalance());
-            currentfield.getOwner().addBalance(currentplayer.getPlayerBalance());
-        }
-    }
-
-    public void removeowner(Player bankruptplayer) {
-        Field field[] = fields;
-        for (int i = 0; i < field.length; i++) {
-            if (field[i] instanceof BuyableField prop) {
-                if (prop.getOwner() == bankruptplayer) {
-                    prop.setOwner(null);
-                }
-            }
-        }
-    }
-
-    public String getTurnMessage() {
-        if (players.size() > 0 && gameStarted) {
-            return "[" + players.get(currentPlayerID).getName() + "'s tur] ";
-        } else {
-            return "";
-        }
-
-    }
-
-    public boolean allColorsOwned(Property currentpropery) {
-        Player properyowner = currentpropery.getOwner();
-        for (Field field : fields) {
-            if (field instanceof Property property) {
-                if (currentpropery.getColor() == property.getColor()) {
-                    if (property.getOwner() == null) {
-                        return false;
-                    }
-                    if (property.getOwner().getID() != properyowner.getID()) {
-                        return false;
+                    mgui.showMessage(Language.getString("2ens"));
+                } else {
+                    player.increaseTurnsinjail();
+                    mgui.showMessage(Language.getString("ikke2ens"));
+                    if (player.getTurnsinjail() > 2) {
+                        player.addBalance(-1000);
+                        player.setJailed(false);
+                        movePlayer(player, die[0] + die[1]);
+                        landOnField(player);
+                        mgui.showMessage(Language.getString("3ture"));
                     }
                 }
             }
-            return true;
         }
-        return false;
-    }
 
-}
+        public ArrayList<Player> getPlayers () {
+            return players;
+        }
+
+        public void buyPropfield (Player currentplayer, BuyableField currentfield){
+            if (currentfield.getPrice() < currentplayer.getPlayerBalance()) {
+                String resuslt = mgui.requestUserButton(Language.getString("prop"), Language.getString("yesTxt"), Language.getString("noTxt"));
+                if (resuslt == Language.getString("yesTxt")) {
+                    currentplayer.addBalance(-currentfield.getPrice());
+                    currentfield.setOwner(currentplayer);
+                    mgui.setOwner(currentfield, currentplayer.getPlayerPosition());
+                }
+            } else {
+                mgui.showMessage("propikkeråd");
+            }
+        }
+
+        public void payRent (Player currentplayer, BuyableField currentfield){
+            mgui.showMessage(Language.getString("payrent") + " " + currentfield.getOwner());
+            if (currentfield instanceof Property property){
+                if (allinColorOwned(property)){
+                    if (currentfield.getRent(0) < currentplayer.getPlayerBalance()) {
+                        currentplayer.addBalance(2*-currentfield.getRent(0));
+                        currentfield.getOwner().addBalance(2*currentfield.getRent(0));
+                    } else {
+                        currentplayer.addBalance(-currentplayer.getPlayerBalance());
+                        currentfield.getOwner().addBalance(currentplayer.getPlayerBalance());
+                    }
+                }
+            }
+            if (currentfield.getRent(0) < currentplayer.getPlayerBalance()) {
+                currentplayer.addBalance(-currentfield.getRent(0));
+                currentfield.getOwner().addBalance(currentfield.getRent(0));
+            } else {
+                currentplayer.addBalance(-currentplayer.getPlayerBalance());
+                currentfield.getOwner().addBalance(currentplayer.getPlayerBalance());
+            }
+        }
+
+        public void removeowner (Player bankruptplayer){
+            Field field[] = fields;
+            for (int i = 0; i < field.length; i++) {
+                if (field[i] instanceof BuyableField prop) {
+                    if (prop.getOwner() == bankruptplayer) {
+                        prop.setOwner(null);
+                    }
+                }
+            }
+        }
+
+        public String getTurnMessage () {
+            if (players.size() > 0 && gameStarted) {
+                return "[" + players.get(currentPlayerID).getName() + "'s tur] ";
+            } else {
+                return "";
+            }
+
+        }
+
+        public boolean allinColorOwned (Property currentpropery){
+            Player properyowner = currentpropery.getOwner();
+            for (Field field : fields) {
+                if (field instanceof Property property) {
+                    if (currentpropery.getColor() == property.getColor()) {
+                        if (property.getOwner() == null) {
+                            return false;
+                        }
+                        if (property.getOwner().getID() != properyowner.getID()) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }

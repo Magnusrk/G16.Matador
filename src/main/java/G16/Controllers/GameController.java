6bdc9bc57e55ba99481ac5172db7a40fcfd9f1d7
Chayanee <s210973@dtu.dk>
2023-01-06 09:18:18 +0100

    Added comments

--- src/main/java/G16/Controllers/GameController.java
+++ src/main/java/G16/Controllers/GameController.java
@@ -59,7 +59,7 @@ public class GameController {
         playTurn();
 
     }
-
+    //Create players, limit amount of player to 3-6 players then add player's names.
     public void setupPlayers(){
         int playerCount = mgui.requestInteger(Language.getString("howManyPlayers"), MIN_PLAYERS, MAX_PLAYERS);
         while (players.size() < playerCount) {
@@ -179,11 +179,14 @@ public class GameController {
             if(currentfield instanceof GoToJail){
                 goToJail(player);
             } else if (currentfield instanceof Property prop) {
+                //if property has no owner, then player can purchase it.
                 if (prop.getOwner()==null) {
                     buyPropfield(player,prop);
                 } else {
+                    //if property has an owner, then player have to pay the rent.
                     payRent(player,prop);
             }
+        //The player land on the Shipping Company's field.
         } else if (currentfield instanceof ShippingCompany ship) {
             if (ship.getOwner()==null){
                 buyShipField(player,ship);
@@ -196,7 +199,7 @@ public class GameController {
             }
 
         }
-
+        //Added 4000 kr to the player's money balance
         public void giveStartMoney (Player player){
             player.addBalance(passStartReward);
 
